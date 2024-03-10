8237a32cca78f7187ad56f9f2017209209cccc2b
shiraz-mian <shirazmian333@gmail.com>
2024-03-10 12:10:05 +0530

    handling errors

---
+++ src/utils/asycHandler.js
@@ -0,0 +1,17 @@
+const asyncHandler = (requestHandler)=>{}
+    (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err) =>next(err))
+    }
+export {asyncHandler}
+
+//another method try catch
+//  const asyncHandler = (fn) => async (req,res,next) =>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 400),json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+//  }
